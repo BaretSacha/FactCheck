377292c4a2cf8f9dcba632888727949c1b7b0b06
BaretSacha <baretsacha@gmail.com>
2024-10-16 14:46:47 +0200

    changement nom

---
+++ Front/fact-check-clients/src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div className="home">
+        <p></p>
+      <header className="header">
+        <div className="hero">
+        <h2>Input a news you want to check!</h2>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
